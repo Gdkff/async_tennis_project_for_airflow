8d94ae09792e5ad781934bc98752bda42a00d9e7
Sergey Gudkov <sergey@inovis.io>
2025-06-01 02:09:46 +0400

    fix dag (for Airflow3)

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -1,15 +1,25 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from datetime import datetime
+from datetime import datetime, timedelta
 from dags_modules.test_process import insert_match
 
 
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
 with DAG(
-    dag_id="load_tennis_data",
-    start_date=datetime(2024, 1, 1),
-    schedule="*/5 * * * *",
+    dag_id='test_dag_airflow_3',
+    default_args=default_args,
+    description='Тестовый даг',
+    schedule_interval="*/5 * * * *",
+    start_date=datetime(2025, 1, 1),
     catchup=False,
-    tags=["tennis"],
 ) as dag:
 
     load_data = PythonOperator(
