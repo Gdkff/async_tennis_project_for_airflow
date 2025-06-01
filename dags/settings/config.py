812f412d5d62e1fa93df220cb986e72adbc6fb8b
Sergey Gudkov <sergey@inovis.io>
2025-06-01 14:41:05 +0400

    try to add dag which record test data to db

--- dags/settings/config.py
+++ dags/settings/config.py
@@ -7,25 +7,26 @@ from dotenv import load_dotenv
 server_ip = "85.190.243.218"
 
 
-# def get_ip():
-#     ip = requests.get("https://api.ipify.org").text
-#     return ip
-#
-#
-# print(get_ip())
-# if get_ip() == server_ip:
-#     env_path = ".env"
-# else:
-#     BASE_DIR = Path(__file__).resolve().parents[2]
-#     env_path = BASE_DIR / ".env.local"
-#
-# print(env_path)
-# if os.path.exists(env_path):
-#     print("Файл есть")
-# else:
-#     print("Файл отсутствует")
-#
-# load_dotenv(dotenv_path=env_path)
+def get_ip():
+    ip = requests.get("https://api.ipify.org").text
+    return ip
+
+
+current_ip = get_ip()
+print(current_ip)
+if current_ip == server_ip:
+    env_path = ".env"
+else:
+    BASE_DIR = Path(__file__).resolve().parents[2]
+    env_path = BASE_DIR / ".env.local"
+
+print(env_path)
+if os.path.exists(env_path):
+    print("Файл есть")
+else:
+    print("Файл отсутствует")
+
+load_dotenv(dotenv_path=env_path)
 
 POSTGRES_HOST = os.getenv('POSTGRES_HOST')
 POSTGRES_DB = os.getenv('POSTGRES_DB')
