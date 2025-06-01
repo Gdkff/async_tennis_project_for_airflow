28d165d01248e77ecd81040739047fba96bf0fa6
Sergey Gudkov <sergey@inovis.io>
2025-06-01 13:49:27 +0400

    try to add dag which record test data to db

--- dags/settings/config.py
+++ dags/settings/config.py
@@ -3,18 +3,28 @@ import socket
 from pathlib import Path
 from dotenv import load_dotenv
 
-local_hostname = "inovis"
-server_hostname = "d3b011aec5af"
 
-hostname = socket.gethostname()
-print(hostname)
+server_ip = "85.190.243.218"
+
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = "127.0.0.1"
+    finally:
+        s.close()
+    return ip
+
+
 BASE_DIR = Path(__file__).resolve().parents[2]
-if hostname.startswith(local_hostname):
-    env_path = BASE_DIR / ".env.local"
-elif hostname.startswith(server_hostname):
-    env_path = BASE_DIR / ".env.server"
+if get_ip() == server_ip:
+    env_path = BASE_DIR / ".env"
 else:
-    exit(hostname)
+    env_path = BASE_DIR / ".env.local"
+
 load_dotenv(dotenv_path=env_path)
 
 POSTGRES_HOST = os.getenv('POSTGRES_HOST')
@@ -23,7 +33,7 @@ POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 
 POSTGRES_CONNECTION_STRING = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}'
+print(POSTGRES_CONNECTION_STRING)
 
-
-if __name__ == '__main__':
-    print(POSTGRES_CONNECTION_STRING)
+# if __name__ == '__main__':
+#     print(POSTGRES_CONNECTION_STRING)
